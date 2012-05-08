ec1a61dd98f48c00acbd10fcb90f00a2b3cd2efb
Chris Dent <chris.dent@gmail.com>
2012-05-08 13:54:51 +0100

    tidy up test file

--- test/test_delete.py
+++ test/test_delete.py
@@ -1,19 +1,30 @@
 
+import shutil
+
 from tiddlyweb.config import config
 from tiddlyweb.model.bag import Bag
 from tiddlyweb.model.tiddler import Tiddler
 
 from tiddlywebplugins.whoosher import init, search
 
+from tiddlywebplugins.utils import get_store
 
-init(config)
+
+def setup_module(module):
+    try:
+        shutil.rmtree('store')
+    except:
+        pass
+    try:
+        shutil.rmtree('indexdir')
+    except:
+        pass
+
+    init(config)
+    module.store = get_store(config)
 
 
 def test_search_unique():
-    from tiddlyweb.store import Store
-    environ = {'tiddlyweb.config': config}
-    store = Store(config['server_store'][0], config['server_store'][1], environ)
-    environ['tiddlyweb.store'] = store
     bag = Bag('bag1')
     store.put(bag)
     tiddler1 = Tiddler('tiddler1', 'bag1')
@@ -24,11 +35,8 @@ def test_search_unique():
     tiddler2.text = 'housesdogscats'
     store.put(tiddler2)
 
-    print 'put the stuff'
-
     tiddlers = list(search(config, 'catsdogshouses'))
 
-    print 'did a search'
     assert len(tiddlers) == 1
     assert tiddlers[0]['id'] == 'bag1:tiddler1'
 
